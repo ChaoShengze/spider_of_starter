2cec9033fcc14d5068e55e012b2ac708bcb78b84
Hope.Phenom <hope.phenom@foxmail.com>
2019-07-09 11:14:20 +0800

    改写代码，添加注释

--- Starter's Web Spider/Starter_s_Web_Spider.py
+++ Starter's Web Spider/Starter_s_Web_Spider.py
@@ -3,24 +3,34 @@
 import urllib.request
 from lxml import etree
 
-url = "https://www.scboy.com/?forum-1.htm"
-response = urllib.request.urlopen(url)
+# 论坛版块总字典
+dict_forum = {}
 
-dict = {}
+# 获取Scboy论坛首页内容
+def getScboy():
+    # url相关
+    baseUrl = "https://www.scboy.com/"
+    url = baseUrl + "?forum-1.htm"
 
-# 获取状态码，200表示成功
-if response.getcode() == 200:
-    txt = response.read()
-    html = etree.HTML(txt, etree.HTMLParser())
-    selector = html.xpath("/html/body/main/div/div/div[1]/div/div[2]/ul/li/div/div[1]/a[1]")
-    print(len(selector))
+    # 获取网页内容
+    response = urllib.request.urlopen(url)
 
-    for t in selector:
-        href = t.attrib['href']
-        if href:
-            if t.text != None:
-                dict[t.text] = href
-            else:
-                dict[t[0].text] = href
+    # 获取状态码，200表示成功
+    if response.getcode() == 200:
+        txt = response.read()
+        # 使用xpath来筛选信息
+        html = etree.HTML(txt, etree.HTMLParser())
+        selector = html.xpath("/html/body/main/div/div/div[1]/div/div[2]/ul/li/div/div[1]/a[1]")
+        print(len(selector))
 
-    print(dict)
+        # 遍历以整理相关信息
+        for t in selector:
+            href = t.attrib['href']
+            if href:
+                if t.text != None:
+                    dict_forum[t.text] = baseUrl + href
+                else:
+                    dict_forum[t[0].text] = baseUrl + href
+
+# 第一步，获取scboy论坛信息
+getScboy()
