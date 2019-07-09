baf6ad08bf7cbebbba05d7885c79db6de1f9780c
Hope.Phenom <hope.phenom@foxmail.com>
2019-07-09 11:06:01 +0800

    能够正确的抓取scboy论坛首页基础信息

--- Starter's Web Spider/Starter_s_Web_Spider.py
+++ Starter's Web Spider/Starter_s_Web_Spider.py
@@ -1,20 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import urllib.request
-# from bs4 import BeautifulSoup
 from lxml import etree
 
 url = "https://www.scboy.com/?forum-1.htm"
-response1 = urllib.request.urlopen(url)
-# 获取状态码，200表示成功
-if response1.getcode() == 200:
-	txt = response1.read()
-	html = etree.HTML(txt, etree.HTMLParser())
-	selector = html.xpath("/html/body/main/div/div/div[1]/div/div[2]/ul/li/div/div[1]/a[1]")
-	print(len(selector))
+response = urllib.request.urlopen(url)
 
-	for t in selector:
-		if t.attrib['href']:
-			print(t.attrib['href'])
-		else:
-			print(t)
+dict = {}
+
+# 获取状态码，200表示成功
+if response.getcode() == 200:
+    txt = response.read()
+    html = etree.HTML(txt, etree.HTMLParser())
+    selector = html.xpath("/html/body/main/div/div/div[1]/div/div[2]/ul/li/div/div[1]/a[1]")
+    print(len(selector))
+
+    for t in selector:
+        href = t.attrib['href']
+        if href:
+            if t.text != None:
+                dict[t.text] = href
+            else:
+                dict[t[0].text] = href
+
+    print(dict)
