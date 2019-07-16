4dd27c42b2e113a6b22ade78ba66aa1799407f28
Hope.Phenom <hope.phenom@foxmail.com>
2019-07-16 15:32:50 +0800

    添加抓取Neo论坛，数据存储准备使用redis

--- Starter's Web Spider/Starter_s_Web_Spider.py
+++ Starter's Web Spider/Starter_s_Web_Spider.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import urllib.request
 from lxml import etree
+import redis
 
 # 论坛版块总字典
 dict_forum = {}
@@ -32,5 +33,48 @@ def getScboy():
                 else:
                     dict_forum[t[0].text] = baseUrl + href
 
+# 获取163官方的内容
+def get163():
+    pass
+
+# 获取NeoTV论坛的内容
+def getNeoTV():
+    # url相关
+    baseUrl = "http://bbs.niuyou5.com/"
+    url = "http://bbs.niuyou5.com/forum-161-1.html"
+    
+    # 获取网页内容
+    response = urllib.request.urlopen(url)
+
+    # 获取状态码，200表示成功
+    if response.getcode() == 200:
+        txt = response.read()
+        # 使用xpath来筛选信息
+        html = etree.HTML(txt, etree.HTMLParser())
+        selector = html.xpath("/html/body/div[5]/div/div[3]/div[3]/div[4]/form/table/tbody/tr/th/a")
+        print(len(selector))
+
+        # 遍历以整理相关信息
+        for t in selector:
+            href = t.attrib['href']
+            if href:
+                if t.text != None:
+                    dict_forum[t.text] = baseUrl + href
+                else:
+                    dict_forum[t[0].text] = baseUrl + href
+
+# sqlite 数据库操作
+def databaseWorker():
+    r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+    r.set('dict', dict_forum)
+    #print(r['name'])
+    print(r.get('dict'))  # 取出键name对应的值
+    #print(type(r.get('name')))
+
 # 第一步，获取scboy论坛信息
 getScboy()
+# 第二步，获取NeoTV论坛信息
+getNeoTV()
+
+print(dict_forum)
+databaseWorker()
